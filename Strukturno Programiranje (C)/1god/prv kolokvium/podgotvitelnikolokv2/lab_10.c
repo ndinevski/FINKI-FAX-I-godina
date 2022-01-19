0899dad6f53d9f800d19172dbf2fe7cda9b8a98e
n1xx1n <ndinevski5@gmail.com>
2022-01-20 00:33:02 +0100

    Update lab_10.c

--- Strukturno Programiranje (C)/1god/prv kolokvium/podgotvitelnikolokv2/lab_10.c
+++ Strukturno Programiranje (C)/1god/prv kolokvium/podgotvitelnikolokv2/lab_10.c
@@ -3,6 +3,11 @@
 Од стандарден влез се читаат два знака z1 и z2. Да се напише програма со која на стандарден излез ќе се испечатат поднизите од секој ред од датотеката составени 
 од знаците што се наоѓаат меѓу z1 и z2 (без нив). Секоја подниза се печати во нов ред.
 Се смета дека секој ред од датотеката точно еднаш ги содржи знаците z1 и z2, знакот z1 секогаш се наоѓа пред знакот z2, а меѓу z1 и z2 секогаш има барем еден знак.
+1122,mleko:100       13     indeks1=4  indeks2=3
+1312,leb:50
+14567,kakao:20
+#
+,:
 */
 
 #include <stdio.h>
@@ -23,32 +28,26 @@ int main(){
     writeToFile();
     FILE *f = fopen("podatoci.txt", "r");
     char a,b;
-    scanf("%c %c", &a, &b);
+    getchar();
+    a=getchar();
+    b=getchar();
     char red[100];
-    int i,indeks1=4,indeks2;
+    int i,indeks1,indeks2;
     while(fgets(red, 100, f)!=NULL){
-        int n=strlen(red)-1;
-        for(i=0;i<n;i++){
-            if(red[i]==a){
+        int flag=0;
+        for(i=0;i<strlen(red)-1;i++){
+            if(red[i]==a && flag==0){
                 indeks1=i;
-                break;
+                flag=1;
             }
-        }
-        //printf("%d", indeks1);
-        for(i=n+1;i>0;i--){
             if(red[i]==b){
                 indeks2=i;
-                break;
             }
         }
-        //printf("%d", indeks2);
-        for(i=indeks2+1;i<n-indeks1;i++){
+        for(i=indeks1+1;i<indeks2;i++){
             printf("%c", red[i]);
         }
         printf("\n");
-        /*for(i=0;i<n;i++){
-            printf("%c", red[i]);
-        }*/
     }
     fclose(f);
     return 0;
