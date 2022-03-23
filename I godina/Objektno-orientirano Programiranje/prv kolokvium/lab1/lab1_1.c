86f3cefbaea1a61d2acede025d2e1169175ae3a5
n1xx1n <ndinevski5@gmail.com>
2022-03-23 20:22:04 +0100

    Update lab1_1.c

--- I godina/Objektno-orientirano Programiranje/prv kolokvium/lab1/lab1_1.c
+++ I godina/Objektno-orientirano Programiranje/prv kolokvium/lab1/lab1_1.c
@@ -2,7 +2,38 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(){
+struct proizvod{
+    char ime[20];
+    float cena;
+    float kolicina;
+};
+typedef struct proizvod proizvod;
 
+void readProizvod(proizvod * p){
+    scanf("%s %f %f", p->ime, &p->cena, &p->kolicina);
+}
+void printProizvod(proizvod  p){
+    printf("%.2f x %.1f = %.2f",p.ime, 1.0*p.cena, 1.0*p.kolicina, p.cena*p.kolicina*1.0);
+}
+float cenaPoProizvod(proizvod p){
+    float cenaPoProizvod=p.cena*p.kolicina*1.0;
+    return cenaPoProizvod;
+}
+
+int main(){
+    int i,n;
+    float sum=0;
+    scanf("%d", &n);
+    proizvod p[100];
+    for(i=1;i<=n;i++){
+        readProizvod(&p[i]);
+    }
+    for(i=1;i<=n;i++){
+        printf("%d. %s\t", i, p[i].ime);
+        printProizvod(p[i]);
+        sum+=cenaPoProizvod(p[i]);
+        printf("\n");
+    }
+    printf("Total: %.2f", sum);
     return 0;
 }
